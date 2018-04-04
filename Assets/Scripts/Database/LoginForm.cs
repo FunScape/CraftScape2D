f23122d79c0da614e6da0129c128df9e70fa935e
Craig Blackburn <cr.blackburn89@gmail.com>
2018-04-03 21:01:19 -0600

    added ability to login and authenticate with server

---
+++ Assets/Scripts/Database/LoginForm.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+using UnityEngine;
+using LitJson;
+
+public class APIRoutes {
+    public const string baseUrl = "localhost:8000";
+    public const string token = baseUrl + "/api/authorize/";
+    public const string users = baseUrl + "/user/";
+    public const string characters = baseUrl + "/character/";
+    public const string axeImage = "https://opengameart.org/sites/default/files/axe2.png";
+}
+
+public class LoginForm : MonoBehaviour {
+
+    public InputField usernameField;
+    public InputField passwordField;
+    public Toggle rememberToggle;
+    public GameObject networkManager;
+
+	void Start() {
+
+        if (rememberToggle.isOn) {
+            LoadCredentials();
+        }
+	}
+
+    void LoadCredentials() {
+        if (PlayerPrefs.HasKey("username")) {
+            usernameField.text = PlayerPrefs.GetString("username");
+        }
+
+        if (PlayerPrefs.HasKey("password")) {
+            passwordField.text = PlayerPrefs.GetString("password");
+        }
+    }
+
+    void SaveCredentials() {
+        PlayerPrefs.SetString("username", usernameField.text);
+        PlayerPrefs.SetString("password", passwordField.text);
+    }
+
+    public void OnClickLogin() {
+        
+        StartCoroutine(Login(usernameField.text, passwordField.text));
+    }
+
+    public void OnClickRememberToggle(bool value) {
+        if (value) {
+            SaveCredentials();
+        } else {
+            PlayerPrefs.DeleteKey("username");
+            PlayerPrefs.DeleteKey("password");
+        }
+        LoadCredentials();
+    }
+
+    IEnumerator Login(string username, string password) {
+
+        string url = string.Format("{0}?username={1}&password={2}", APIRoutes.token, username, password);
+
+        UnityWebRequest request = UnityWebRequest.Post(url, new Dictionary<string, string>());
+
+        yield return request.SendWebRequest();
+        if (request.isNetworkError || request.isHttpError)
+		{
+            Debug.Log(request.error);
+		}
+		else
+		{
+            if (rememberToggle.isOn)
+            {
+                SaveCredentials();
+            }
+
+			Debug.Log("Authentication successful!");
+            JsonData response = JsonMapper.ToObject(request.downloadHandler.text);
+            networkManager.SetActive(true);
+            gameObject.SetActive(false);
+		}
+    }
+
+	public IEnumerator GetTexture(string url, SpriteRenderer renderer) {
+
+		Texture2D texture;
+		texture = new Texture2D (1, 1);
+
+		using (WWW www = new WWW(url))
+		{
+			yield return www;
+			www.LoadImageIntoTexture (texture);
+			Sprite sprite = Sprite.Create (texture, 
+				new Rect(0, 0, texture.width, texture.height), 
+				Vector2.one
+			);
+			renderer.sprite = sprite;
+		}
+
+	}
+
+
+	
+}
