d9b6d4c3dadd745a9293494084f43355abceaa90
Nate <nate.lovell@aggiemail.usu.edu>
2018-04-12 20:10:00 -0600

    Created prefab for containing skill menu. Created script for creating and controlling skills and skill menu, attached script to player prefab. The menu doesn't appear when the appropriate key is pressed, and I'm not sure why. No console errors as far as I can tell. I'll figure that out later.

---
+++ Assets/Scripts/SkillScripts/PlayerSkillController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSkillController : MonoBehaviour {
+
+    public GameObject skillMenuPanelPrefab;
+
+    public GameObject skillMenuPanel;
+
+    bool showSkillMenu;
+
+    float cameraHeight;
+    float cameraWidth;
+
+    protected const string treePanelName = "TreePanel";
+    protected const string selectionPanelName = "SelectionPanel";
+    protected const string selectionImageName = "SelectionImage";
+    protected const string selectionTextName = "SelectionText";
+    protected const string selectionTextBackgroundName = "SelectionTextBackground";
+    protected const string ingredientsPanelName = "IngredientsPanel";
+    protected const string selectionButtonName = "SelectionButton";
+
+	// Use this for initialization
+	void Start () {
+        cameraHeight = Camera.main.pixelHeight;
+        cameraWidth = Camera.main.pixelWidth;
+
+        showSkillMenu = false;
+
+        GameObject mainCanvas = GameObject.FindWithTag("MainCanvas");
+
+        skillMenuPanel = Instantiate(skillMenuPanelPrefab, new Vector3(cameraWidth * 2, 0f, 0f), Quaternion.identity, mainCanvas.transform);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.P))
+            ToggleSkillMenu();
+
+	}
+
+    void ToggleSkillMenu()
+    {
+        showSkillMenu = !showSkillMenu;
+
+        if (showSkillMenu)
+            skillMenuPanel.transform.localPosition = new Vector3(-cameraWidth / 3, 0f, 0f);
+        else
+            skillMenuPanel.transform.localPosition = new Vector3(-cameraWidth * 2, 0f, 0f);
+
+        return;
+    }
+}
